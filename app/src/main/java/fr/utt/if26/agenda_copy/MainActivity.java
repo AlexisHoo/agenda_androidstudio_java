2d2933c6d4aa34b6e17ffb651a38630e27876f59
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2023-11-11 21:56:12 +0100

    first

---
+++ app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.utt.if26.agenda_copy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
