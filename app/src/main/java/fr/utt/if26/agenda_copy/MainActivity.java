6491fc9d79e6cb2461e9be5f193511656cdf6517
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2023-11-11 23:01:46 +0100

    calendar

--- app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
+++ app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -11,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Date today = new Date();
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR,1);
+
+        CalendarPickerView datePicker = findViewById(R.id.calendar);
+        datePicker.init(today, nextYear.getTime()).withSelectedDate(today);
+
+        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                //String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
+                Calendar calSelected = Calendar.getInstance();
+                calSelected.setTime(date);
+                String selectDate = "" + calSelected.get(Calendar.DAY_OF_MONTH)
+                        + " " + (calSelected.get(Calendar.MONTH) + 1)
+                        + " " + (calSelected.get(Calendar.YEAR));
+
+
+
+                Toast.makeText(MainActivity.this, selectDate, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDateUnselected(Date date) {
+
+            }
+        });
+
+
     }
+
+
 }
