10c5763e32273a56d5f01f51a45f5b82d246f4d3
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2023-11-17 17:22:39 +0100

    monthly calendar bases small adds

--- app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
+++ app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
@@ -1,17 +1,47 @@
 package fr.utt.if26.agenda_copy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    // 5.40
+    // 6.33
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
 
+    }
+
+    private void setMonthView() {
+
+        monthYearText.setText(monthYearFromDate(selectedDate));
+    }
+
+    private String monthYearFromDate(LocalDate date){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    private void initWidgets() {
+
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTv);
 
     }
 
