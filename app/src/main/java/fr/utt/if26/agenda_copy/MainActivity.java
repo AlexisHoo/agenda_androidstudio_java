a4fc6149014bcbf6834aab8898b45e1cb1488d38
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2023-11-20 15:55:57 +0100

    monthly calendar bases done

--- app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
+++ app/src/main/java/fr/utt/if26/agenda_copy/MainActivity.java
@@ -1,21 +1,27 @@
 package fr.utt.if26.agenda_copy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements calendarViewAdapter.onItemListener{
     Button button;
     // 6.33
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private LocalDate selectedDate;
+    private Button previousMonth, nextMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,57 @@ public class MainActivity extends AppCompatActivity {
         selectedDate = LocalDate.now();
         setMonthView();
 
+        previousMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedDate = selectedDate.minusMonths(-1);
+                setMonthView();
+            }
+        });
+
+        nextMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedDate = selectedDate.plusMonths(1);
+                setMonthView();
+            }
+        });
+
     }
 
     private void setMonthView() {
 
         monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        calendarViewAdapter calendarAdapter = new calendarViewAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+
+        for(int i=1; i <= 42;i++){
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
+                daysInMonthArray.add("");
+
+            }
+            else{
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+
+        return daysInMonthArray;
     }
 
     private String monthYearFromDate(LocalDate date){
@@ -40,10 +92,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void initWidgets() {
 
+        previousMonth = findViewById(R.id.previousMonth);
+        nextMonth = findViewById(R.id.nextMonth);
         calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
         monthYearText = findViewById(R.id.monthYearTv);
 
     }
+/*
+    public void previousMonth(View view){
+        selectedDate = selectedDate.minusMonths(-1);
+        setMonthView();
+    }
+
+    public void nextMonth(View view){
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+ */
 
 
+    @Override
+    public void onItemClick(int position, String dayText) {
+
+        if(dayText.equals("")){
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
+    }
 }
